cf895593bfbcbf2875dabd415080cf540443efe5
Andrea Sestito <andrea3email@gmail.com>
2022-04-30 17:03:18 +0200

    CV

--- src/Home.js
+++ src/Home.js
@@ -3,16 +3,16 @@ import profile from "../src/images/profile.jpg"
 function Home() {
   return (
   <div>
-      <div class="body">
-        <img src={profile} style={{padding:"20px"}}id="profile"></img>
-          <div class="section">
-            <h1>About</h1>
-            <h3>
-              Focused on software optimization, evolution and improvement supporting development life cycle along with Test Automation tools.
-              Interested in software quality and correctness, detailing test development environment according to project requirements.
-              Open-minded towards modern frameworks and applications and constantly up to date in technological innovation. 
-            </h3>
-          </div>
+      <div style={{padding:"20px"}} class="content">
+        <img src={profile}  style={{float: "left"}}width="250vw" height="300vh"></img>
+        <div class="section" style={{float: "right"}}>
+          <h1>About</h1>
+          <h3>
+            Focused on software optimization, evolution and improvement supporting development life cycle along with Test Automation tools.
+            Interested in software quality and correctness, detailing test development environment according to project requirements.
+            Open-minded towards modern frameworks and applications and constantly up to date in technological innovation. 
+          </h3>
+        </div>
       </div>
   </div>
   );
