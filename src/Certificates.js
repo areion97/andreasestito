ad21e1f2794824d9f819db6089b2364d07d96a68
Andrea Sestito <andrea.sestito@outlook.com>
2023-03-04 11:30:39 +0100

    adjusted footer

---
+++ src/Certificates.js
@@ -0,0 +1,38 @@
+import fce_certificate from "./images/FCE.jpg"
+import ibm_certificate from "./images/IBM_Certificate.jpg"
+import './css/global.css';
+import Footer from "./Footer";
+function Certificates() {
+ 
+  return (
+  <div>
+    <div class="content">
+      <div className="section">
+      <table>
+        <tr>
+          <td>
+            <h1 class="p-title">
+              First Certificate in English
+            </h1>
+            <img src={fce_certificate} alt="FCE Certificate" className="responsive-image"></img>
+          </td>
+          <td>
+            <h1 class="p-title">
+              IBM Full Stack Software Developer
+            </h1>
+            <img src={ibm_certificate} alt="ibmCertificate" className="responsive-image"></img>
+
+          </td>
+        </tr>
+      </table>
+      </div>
+  
+    </div>
+    <Footer/>
+
+  </div>
+  );
+}
+
+export default Certificates;
+
---
+++ src/Footer.js
@@ -0,0 +1,28 @@
+import React from "react";
+import instagram_logo from "../src/images/instagram_logo.png"
+import linkedin_logo from "../src/images/linkedin-logo.png"
+import facebook_logo from "../src/images/fb-logo.png"
+
+function Footer() {
+
+
+    return (
+        <footer>
+            <table className="centered">
+            <tr className="centered-1">
+                <td class="footer-icon">
+                <a href="https://www.facebook.com/sestitoandre/"><img  src={facebook_logo} alt="fb" style={{backgroundColor: "none"}} width="58px" height="58px"></img></a>
+                </td>
+                <td class="footer-icon">
+                <a href="https://www.linkedin.com/in/andreasestito/"><img src={linkedin_logo} alt="lnkding" style={{backgroundColor: "none"}} width="58px" height="58px"></img></a>
+                </td>
+                <td class="footer-icon">
+                <a href="https://www.instagram.com/sestitoandre/"><img src={instagram_logo} alt="igram" style={{backgroundColor: "none"}} width="58px" height="58px"></img></a>
+                </td>
+            </tr>
+            </table>
+        </footer>
+    )
+}
+
+export default Footer;
