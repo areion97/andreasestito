fd7d251ce01bc5c4dc002d72f1159aa25e1e1c88
Andrea Sestito <andrea.sestito@outlook.com>
2023-03-18 16:21:03 +0100

    cs50 certificate

---
+++ src/Certificates.js
@@ -0,0 +1,47 @@
+import fce_certificate from "./images/FCE.jpg"
+import ibm_certificate from "./images/IBM_Certificate.jpg"
+import cs50_certificate from "./images/CS50x.png"
+
+import './css/global.css';
+import Footer from "./Footer";
+function Certificates() {
+ 
+  return (
+  <div>
+    <div class="content">
+      <div className="section">
+      <table>
+        <tr>
+          <td>
+            <h1 className="p-title">
+              First Certificate in English
+            </h1>
+
+            <img src={fce_certificate} alt="FCE Certificate" className="responsive-image"></img>
+          </td>
+          <td>
+            <h1 className="p-title">
+              IBM Full Stack Software Developer
+            </h1>
+
+            <img src={ibm_certificate} alt="ibmCertificate" className="responsive-image"></img>
+            <h1 className="p-title">
+              CS50x
+            </h1>
+
+            <img src={cs50_certificate} alt="cs50-certificate" className="responsive-image"></img>
+          </td>
+  
+        </tr>
+      </table>
+      </div>
+  
+    </div>
+    <Footer/>
+
+  </div>
+  );
+}
+
+export default Certificates;
+
---
+++ src/Home.js
@@ -0,0 +1,43 @@
+import profile from "../src/images/profile.jpg"
+import Footer from './Footer';
+
+function Home() {
+  return (
+    <div>
+      <div class="content">
+          <table>
+            <tr>
+              <td className="centered-1">
+                <img alt="me" style={{ border: "4px solid rgb(140,0,0)", marginTop:"50px", marginLeft: "30px"}} src={profile} width="250" height="333.33"></img>
+             </td>
+             <td style={{verticalAlign: "middle"}}>
+                <div class="section" style={{ float: "top", width: "80%"}}>
+                  <h1>About</h1>   
+                  <h3>
+                    Focused on software optimization, evolution and improvement supporting development life cycle along with Test Automation tools.
+                    Interested in software quality and correctness, detailing test development environment according to project requirements.
+                    Open-minded towards modern frameworks and applications and constantly up to date in technological innovation. 
+                  </h3>
+                </div>
+             </td>
+            </tr>
+            <tr>
+              <td>
+              <div class="section">
+               
+              </div>
+              </td>
+              <td>
+              </td>
+            </tr>
+          </table>
+
+      </div>
+      <Footer/>
+
+    </div>
+
+  );
+}
+
+export default Home;
