4d5a3450cca6141c863504498560c2d943756a2d
Andrea Sestito <andrea.sestito@outlook.com>
2024-05-11 13:37:11 +0200

    removed linkedin badge

--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,6 @@ import profile from "../src/images/me.jpg"
 import { useEffect } from "react";
 
 function Home() {
-  useEffect(() => {
-    const script = document.createElement("script");
-
-    script.src = "https://platform.linkedin.com/badges/js/profile.js";
-    script.async = true;
-
-    document.body.appendChild(script);
-  },[]);
   return (
     
     <>
@@ -23,9 +15,7 @@ function Home() {
               <h1>Introduction</h1>   
               <h3>
                 My name is Andrea and I am from Parma, Italy. I created this website to introduce what are my main hobbies and passions.<br/><br/>
-              </h3>
-              <div class="badge-base LI-profile-badge" data-locale="it_IT" data-size="large" data-theme="light" data-type="VERTICAL" data-vanity="andreasestito" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://it.linkedin.com/in/andreasestito?trk=profile-badge"><p></p></a></div>
-   
+              </h3>   
             </div>
 
           </div>
