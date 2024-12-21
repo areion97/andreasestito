f2e32cbe12701160f8ba35d60db385b39cae3a2b
areion97 <andrea.sestito@outlook.com>
2024-12-21 12:05:15 +0100

    added github in the footer

--- src/js/Footer.js
+++ src/js/Footer.js
@@ -2,6 +2,8 @@ import React from 'react';
 import instagram_logo from '../images/instagram_logo.png';
 import linkedin_logo from '../images/linkedin-logo.png';
 import facebook_logo from '../images/fb-logo.png';
+import github_logo from '../images/github-mark.svg';
+
 import PropTypes from 'prop-types';
 
 function Footer(props) {
@@ -25,6 +27,17 @@ function Footer(props) {
               <img
                 src={instagram_logo}
                 alt="instagram"
+                style={{ backgroundColor: 'none' }}
+                width="40px"
+                height="40px"
+              ></img>
+            </a>
+          </td>
+          <td>
+            <a href="https://github.com/areion97">
+              <img
+                src={github_logo}
+                alt="github"
                 style={{ backgroundColor: 'none' }}
                 width="40px"
                 height="40px"
--- src/js/Hobby.js
+++ src/js/Hobby.js
@@ -1,22 +1,27 @@
+import React from 'react';
 import garden_microsoft from '../images/garden-microsoft.jpg';
 
 function Hobby() {
   return (
     <div className="container">
-    <div style={{ backgroundColor: '#961a36', width: '100%' }}>
-      <div className="content">
-        <table>
-          <tr className="flexbox-column">
-            <td>
-              <div className="section-50">
-                <h1>Photography</h1>
-                <img className="flex-image" src={garden_microsoft}></img>
-              </div>
-            </td>
-          </tr>
-        </table>
+      <div style={{ backgroundColor: '#961a36', width: '100%' }}>
+        <div className="content">
+          <table>
+            <tr className="flexbox-column">
+              <td>
+                <div className="section-50">
+                  <h1>Photography</h1>
+                  <img
+                    className="flex-image"
+                    src={garden_microsoft}
+                    alt="Garden Microsoft"
+                  />
+                </div>
+              </td>
+            </tr>
+          </table>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
